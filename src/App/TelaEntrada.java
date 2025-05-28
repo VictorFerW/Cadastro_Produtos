aa8caee4b2b65243ff13fdc394a50862a6a56401
Mvyf <Luandemiguel2018@outlook.com>
2025-05-28 19:35:38 -0300

    Telas

---
+++ src/App/TelaEntrada.java
@@ -0,0 +1,43 @@
+package App;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TelaEntrada {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaEntrada window = new TelaEntrada();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaEntrada() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
